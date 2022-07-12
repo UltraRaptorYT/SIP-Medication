63fb06fd1f0cedb308fbc544dbc1d272b5a0524e
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2022-07-12 13:30:16 +0800

    .

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+
+var hostname = "localhost";
+var port = 3000;
+
+var app = express();
+
+app.use(function (req, res, next) {
+  if (req.method != "GET") {
+    res.type(".html");
+    var msg =
+      "<html><body>This server only serves web pages with GET!</body></html>";
+    res.end(msg);
+  } else {
+    next();
+  }
+});
+
+app.use(serveStatic(__dirname + "/public"));
+
+app.listen(port, hostname, function () {
+  console.log(`Server hosted at http://${hostname}:${port}`);
+});
